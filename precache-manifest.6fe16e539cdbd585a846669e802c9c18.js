c1788566b39eb8c30fa390c078774ea3f19ea3a7
Devansh Dalal <devansh.dalal@thoughtspot.com>
2020-03-07 10:10:36 +0530

    Updates

---
+++ precache-manifest.6fe16e539cdbd585a846669e802c9c18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bce15d031435811ee1106ae09a9ec7d",
+    "url": "/news-recommendation-frontend/index.html"
+  },
+  {
+    "revision": "43031131190829f384b6",
+    "url": "/news-recommendation-frontend/static/css/2.de49db6c.chunk.css"
+  },
+  {
+    "revision": "9f9a5179bd768a7788bd",
+    "url": "/news-recommendation-frontend/static/css/main.9e74beb3.chunk.css"
+  },
+  {
+    "revision": "43031131190829f384b6",
+    "url": "/news-recommendation-frontend/static/js/2.a6db2dfe.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/news-recommendation-frontend/static/js/2.a6db2dfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f9a5179bd768a7788bd",
+    "url": "/news-recommendation-frontend/static/js/main.471697bb.chunk.js"
+  },
+  {
+    "revision": "304f9ece60e394a2ee2f",
+    "url": "/news-recommendation-frontend/static/js/runtime-main.f1ca0615.js"
+  },
+  {
+    "revision": "30793e60f217d6c9510dede5d9562a92",
+    "url": "/news-recommendation-frontend/static/media/logo.30793e60.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/news-recommendation-frontend/precache-manifest.c91b11a7620c57313ce8c701ec66be71.js"
+  "/news-recommendation-frontend/precache-manifest.6fe16e539cdbd585a846669e802c9c18.js"
 );
 
 self.addEventListener('message', (event) => {
